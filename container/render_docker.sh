f92f2536f266b740f74dd42f8059194ba64a450e
Thomas Chaton <thomas.chaton.ai@gmail.com>
2020-08-16 02:11:14 +0100

    remove useless file

--- container/render_docker.sh
+++
@@ -1,4 +0,0 @@
-requirements=$(cat requirements.txt | python3 -c "import sys, json; print(''.join(['RUN pip install '+l for l in sys.stdin]))")
-docker_template=$(cat docker_template.txt) 
-output=$(sed -i -e "s/{{requirements}}/$requirements/g" $docker_template)
-echo $output
